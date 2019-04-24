fae4f6700ebda7ae83851ea966b19f8043158dcf
Alexandre <aas@unochapeco.edu.br>
2019-04-24 07:10:17 -0300

    Inclusão dos fontes da biblioteca

---
+++ app/src/test/java/com/s/d/a/a/androidutils/HttpHelperTest.java
@@ -0,0 +1,16 @@
+package com.s.d.a.a.androidutils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpHelperTest {
+
+    @Test
+    public void doGet() {
+    }
+
+    @Test
+    public void doGetBitmap() {
+    }
+}
