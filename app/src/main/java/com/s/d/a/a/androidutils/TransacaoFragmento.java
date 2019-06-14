2f4acab55d0e601d22abafe76f3c1f40b8c78ac7
Alexandre <aas@unochapeco.edu.br>
2019-06-14 09:30:36 -0300

    Alterações no TransacaoFragmento.java

--- app/src/main/java/com/s/d/a/a/androidutils/TransacaoFragmento.java
+++ app/src/main/java/com/s/d/a/a/androidutils/TransacaoFragmento.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
-public abstract class TransacaoFragmento extends AsyncTask<Void, Void, Boolean> {
+public class TransacaoFragmento extends AsyncTask<Void, Void, Boolean> {
     private static final String TAG = "androidutils";
     private final Context context;
     private final Fragment fragment;
